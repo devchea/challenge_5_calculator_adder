1bcd59c34ebba2561c6837acf796ce0df4ac78e2
devchea <34663914+devchea@users.noreply.github.com>
2021-01-12 13:36:58 -0800

    updates

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 function App() {
+  const [firstNum, setFirstNum] = useState(0);
+  console.log(firstNum);
+
   return (
     <div className="App">
       <h1>Adding Two Numbers</h1>
 
       <div className="number-inputs">
-        <input type="number" placeholder="0" />
+        <input
+          type="number"
+          placeholder="0"
+          onChange={(e) => setFirstNum(e.target.value)}
+        />
         <input type="number" placeholder="0" />
       </div>
 
-      <button>Add Them!</button>
+      <button>+</button>
+      <button>-</button>
+      <button>x</button>
+      <button>/</button>
 
       <h2>0</h2>
 
@@ -22,5 +32,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
